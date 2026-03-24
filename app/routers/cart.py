82e4cbaff76332b26627598efe1bb5458e82fa23
Shreyash-Bhimte <shreyash.bhimte99@gmail.com>
2026-03-24 20:07:15 +0530

    add cart endpoints

--- app/routers/cart.py
+++ app/routers/cart.py
@@ -1,12 +1,47 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from app.database import get_db
 from sqlalchemy.orm import Session
-from app.models import User
+from app.models import Cart,CartItem, User
 from app.dependencies import get_current_user
+from app.schemas import CartItemCreate,ProductCreate
+
 
 router = APIRouter()
 
+
+
+@router.post("/cart",response_model=CartItemCreate)
+def add_item( item: CartItemCreate, db: Session = Depends(get_db),current_user: User = Depends(get_current_user)):
+    cart = db.query(Cart).filter(Cart.user_id == current_user.id).first()
+    if not cart:
+        cart = Cart(user_id= current_user.id)
+        db.add(cart)
+        db.commit()
+        db.refresh(cart)
+    new_item = CartItem(cart_id=cart.id, product_id= item.product_id, quantity= item.quantity)
+    db.add(new_item)
+    db.commit()
+    db.refresh(new_item)
+
+    return new_item
+
 @router.get("/cart")
-def get_cart(db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
-    # current_user is the logged in user
-    pass
+def get_all_items(db: Session = Depends(get_db), current_user: User = Depends(get_current_user) ):
+    cart = db.query(Cart).filter(Cart.user_id == current_user.id).first()
+    if not cart:
+        raise HTTPException(status_code=404, detail="Cart not found")
+    
+    items = db.query(CartItem).filter(CartItem.cart_id == cart.id).all()
+    return items
+
+
+@router.delete("/cart/{id}")
+def delete_products(id: int ,db: Session = Depends(get_db)):
+    product_by_id = db.query(Product).filter(Product.id == id).first()
+    if not product_by_id:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="product not found")
+
+    db.delete(product_by_id)
+    db.commit()
+
+    return {"message": "product deleted"}
--- app/routers/products.py
+++ app/routers/products.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from app.database import get_db
 from sqlalchemy.orm import Session
 from app.models import Product
@@ -13,7 +13,7 @@ def get_products(db: Session = Depends(get_db)):
     return all_products
 
 @router.post("/products",response_model=ProductResponse)
-def post_products(product: ProductCreate ,db: Session = Depends(get_db)):
+def create_products(product: ProductCreate ,db: Session = Depends(get_db)):
     new_product = Product(name= product.name ,description= product.description ,price= product.price ,stock_quantity= product.stock_quantity)
     db.add(new_product)
     db.commit()
@@ -22,7 +22,36 @@ def post_products(product: ProductCreate ,db: Session = Depends(get_db)):
     return new_product
 
 
-@router.get("/products/{id}")
-def get_product(product: ProductCreate ,db: Session = Depends(get_db)):
-    product_by_name = db.query(Product).filter(Product.name == product.name).first()
-    return product_by_name
+@router.get("/products/{id}", response_model=ProductResponse)
+def get_product(id: int,db: Session = Depends(get_db)):
+    product_by_id = db.query(Product).filter(Product.id == id).first()
+    if not product_by_id:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="product not found")
+    return product_by_id
+
+@router.put("/products/{id}",response_model=ProductResponse)
+def update_products(id: int,product: ProductCreate ,db: Session = Depends(get_db)):
+    product_by_id = db.query(Product).filter(Product.id == id).first()
+    if not product_by_id:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="product not found")
+    
+    product_by_id.name= product.name 
+    product_by_id.description= product.description 
+    product_by_id.price= product.price 
+    product_by_id.stock_quantity= product.stock_quantity
+
+    db.commit()
+    db.refresh(product_by_id)
+
+    return product_by_id
+
+@router.delete("/products/{id}")
+def delete_products(id: int ,db: Session = Depends(get_db)):
+    product_by_id = db.query(Product).filter(Product.id == id).first()
+    if not product_by_id:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="product not found")
+
+    db.delete(product_by_id)
+    db.commit()
+
+    return {"message": "product deleted"}

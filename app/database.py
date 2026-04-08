0b271820e223b672b9919f6839fd17569f4a98b4
Shreyash-Bhimte <shreyash.bhimte99@gmail.com>
2026-04-08 17:41:55 +0530

    Fix PostgreSQL password

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 # If not set, use the Railway PostgreSQL URL directly
 if not DATABASE_URL:
-    DATABASE_URL = "postgresql://postgres:SHR12febpg@maglev.proxy.rlwy.net:17381/railway"
+    DATABASE_URL = "postgresql://postgres:fMpjprTyNXneyPfwdGQIzDxtaGPLOKQt@maglev.proxy.rlwy.net:17381/railway"
 
 engine = create_engine(DATABASE_URL)
 
